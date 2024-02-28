aed1f5037848d7ea5919820a9fdccc857b14f039
maffinnn <biansiyuanlynn@gmail.com>
2024-02-28 12:15:05 +0800

    adding cache

--- lib/golang/cache/test/test.go
+++ lib/golang/cache/test/test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"fmt"
+	"net/http"
+	"flag"
 	"strconv"
 
 	"distributed-file-system/lib/golang/cache"
@@ -86,6 +88,65 @@ func testCache() {
 	}
 }
 
+func createGroup() *cache.Group {
+	return cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+}
+
+func startCacheServer(addr string, addrs []string, c *cache.Group) {
+	peers := cache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	c.RegisterPeers(peers)
+	log.Println("Cache server is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, c *cache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := c.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+
+}
+
 func main() {
-	testHashing()
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Cache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	cache := createGroup()
+	if api {
+		go startAPIServer(apiAddr, cache)
+	}
+	startCacheServer(addrMap[port], []string(addrs), cache)
 }
